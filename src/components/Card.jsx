b292bdabf0281f62c60b6fbf3bd40bf54a9f7ba2
lior <liorazar456@gmail.com>
2025-06-29 09:37:53 +0300

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Card({ value, isFlipped, onClick }) {
+  return (
+    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={onClick}>
+      <div className="card-inner">
+        <div className="card-front">❓</div>
+        <div className="card-back">{value}</div>
+      </div>
+    </div>
+  );
+}
